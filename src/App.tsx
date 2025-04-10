60532cd5dce0574d0ab8b9fdf446b3c2192fff6c
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-04-10 22:44:44 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Layout from "./components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provide";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
---
+++ src/hooks/use-geolocation.ts
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import { Coordinates } from '../api/types';
+interface GeolocationState{
+    coordinates:Coordinates|null;
+    error: string|null;
+    isLoading:boolean;
+}
+
+export function useGeolocation (){
+    const [locationData,setLocationData]=useState<GeolocationState>({
+        coordinates:null,
+        error:null,
+        isLoading:false,    
+    });
+
+const getLocation=()=>{
+setLocationData((prev)=>({
+    ...prev, isLoading:true,error:null
+}));
+
+if(!navigator.geolocation){
+ setLocationData({
+    coordinates:null,
+    error:"Geolocation is not supported",
+    isLoading:false,
+ });
+    return;
+}
+
+navigator.geolocation.getCurrentPosition((position)=>{
+    setLocationData({
+        coordinates:{
+            lat:position.coords.latitude,
+            lon:position.coords.longitude,
+        },
+        error:null,
+        isLoading:false,    
+    })
+},(error)=>{
+    let errorMessage: string;
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+            errorMessage="Location permission denied. Please enable location access.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            errorMessage="Location information is unavailable";
+            break;
+        case error.TIMEOUT:
+            errorMessage="The request to get user location timed out";
+            break;
+       default:
+            errorMessage="An unknown error occurred";
+            break;
+    }
+
+    setLocationData({
+        coordinates:null,
+        error:errorMessage,
+        isLoading:false,
+    });
+},{
+    enableHighAccuracy:true,
+    timeout:5000,
+    maximumAge:0,
+})
+
+}
+
+useEffect(()=>{
+getLocation();
+},[])
+
+return{
+    ...locationData,
+    getLocation,
+}
+
+}
